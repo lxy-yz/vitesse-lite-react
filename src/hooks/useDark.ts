6c930c9c619f44e991ca1f9a37bb4c794ebd6876
lxy-yz <lxy.dev.yz@gmail.com>
2022-03-21 00:16:06 -0700

    fix: dark mode fixes

--- src/hooks/useDark.ts
+++ src/hooks/useDark.ts
@@ -1,6 +1,13 @@
 export default function useDark() {
   const { isDarkMode: isDark, toggle: toggleDark } = useDarkMode()
 
+  useEffect(() => {
+    if (isDark)
+      document.documentElement.classList.add('dark')
+    else
+      document.documentElement.classList.remove('dark')
+  }, [isDark])
+
   return {
     isDark,
     toggleDark,
